f3b43eb44ad7916657e6d66ce0237ea49f8827f4
XFL <18720909751@163.com>
2017-06-11 21:23:51 +0800

    dubbo测试（消费者端）

--- src/main/java/com/xfl/boot/TestController.java
+++ src/main/java/com/xfl/boot/TestController.java
@@ -1,8 +1,10 @@
 package com.xfl.boot;
 
+import com.xfl.provider.service.IDemoService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
@@ -16,11 +18,14 @@ import java.util.Map;
 @RestController
 @RequestMapping(value = "/test")
 public class TestController {
+    @Resource
+    private IDemoService demoService;
     @RequestMapping(value = "/boot")
     public Map<String,Object> test(HttpServletRequest servletRequest, HttpServletResponse servletResponse){
         System.out.println("Test");
         Map<String,Object> result = new HashMap<>();
         result.put("msg","test");
+        System.out.println(demoService.sayHello("Boot Test"));
         return result;
     }
 }
---
+++ src/main/java/com/xfl/provider/service/IDemoService.java
@@ -0,0 +1,10 @@
+package com.xfl.provider.service;
+
+/**
+ * Created by XFL
+ * time on 2017/6/11 20:56
+ * description:
+ */
+public interface IDemoService {
+    String sayHello(String var1);
+}
