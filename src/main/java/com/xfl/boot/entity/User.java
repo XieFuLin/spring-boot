4c0e2e8670477eb18195a8082b8c35a281982136
XFL <18720909751@163.com>
2018-03-20 23:12:34 +0800

    解决Long类型转换成json时精度丢失问题

--- src/main/java/com/xfl/boot/entity/User.java
+++ src/main/java/com/xfl/boot/entity/User.java
@@ -1,6 +1,8 @@
 package com.xfl.boot.entity;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
 /**
  * Created by XFL
@@ -12,7 +14,9 @@ public class User {
     private String name;
     private String age;
     private String desc;
-
+    //将Long类型系列化成String避免精度丢失
+    @JsonSerialize(using = ToStringSerializer.class)
+    private Long userId;
     public String getName() {
         return name;
     }
@@ -37,12 +41,21 @@ public class User {
         this.desc = desc;
     }
 
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
     @Override
     public String toString() {
         return "User{" +
                 "name='" + name + '\'' +
                 ", age='" + age + '\'' +
                 ", desc='" + desc + '\'' +
+                ", userId=" + userId +
                 '}';
     }
 }
