09630236dea9111dca6ce6ae08cdb21ba6792059
XFL <18720909751@163.com>
2018-05-13 19:30:01 +0800

    RSA增加公钥加密私钥解密方法

--- src/main/java/com/xfl/boot/common/utils/RSAEncryptUtils.java
+++ src/main/java/com/xfl/boot/common/utils/RSAEncryptUtils.java
@@ -77,8 +77,10 @@ public class RSAEncryptUtils {
      * 私钥解密
      * </p>
      *
-     * @param encryptedData 已加密数据
-     * @param privateKey    私钥(BASE64编码)
+     * @param encryptedData
+     *            已加密数据
+     * @param privateKey
+     *            私钥(BASE64编码)
      * @return
      * @throws Exception
      */
@@ -115,8 +117,10 @@ public class RSAEncryptUtils {
      * 公钥加密
      * </p>
      *
-     * @param data      源数据
-     * @param publicKey 公钥(BASE64编码)
+     * @param data
+     *            源数据
+     * @param publicKey
+     *            公钥(BASE64编码)
      * @return
      * @throws Exception
      */
@@ -148,15 +152,92 @@ public class RSAEncryptUtils {
         out.close();
         return Base64Utils.encodeToString(encryptedData);
     }
-
+    /** */
+    /**
+     * <p>
+     * 公钥解密
+     * </p>
+     *
+     * @param encryptedData 已加密数据
+     * @param publicKey     公钥(BASE64编码)
+     * @return
+     * @throws Exception
+     */
+    public static String decryptByPublicKey(byte[] encryptedData, String publicKey)
+            throws Exception {
+        byte[] keyBytes = Base64Utils.decode(publicKey);
+        X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
+        Key publicK = keyFactory.generatePublic(x509KeySpec);
+        Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
+        cipher.init(Cipher.DECRYPT_MODE, publicK);
+        int inputLen = encryptedData.length;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        int offSet = 0;
+        byte[] cache;
+        int i = 0;
+        // 对数据分段解密
+        while (inputLen - offSet > 0) {
+            if (inputLen - offSet > MAX_DECRYPT_BLOCK) {
+                cache = cipher.doFinal(encryptedData, offSet, MAX_DECRYPT_BLOCK);
+            } else {
+                cache = cipher.doFinal(encryptedData, offSet, inputLen - offSet);
+            }
+            out.write(cache, 0, cache.length);
+            i++;
+            offSet = i * MAX_DECRYPT_BLOCK;
+        }
+        byte[] decryptedData = out.toByteArray();
+        out.close();
+        return new String(decryptedData, charset);
+    }
+    /** */
+    /**
+     * <p>
+     * 私钥加密
+     * </p>
+     *
+     * @param data       源数据
+     * @param privateKey 私钥(BASE64编码)
+     * @return
+     * @throws Exception
+     */
+    public static String encryptByPrivateKey(byte[] data, String privateKey)
+            throws Exception {
+        byte[] keyBytes = Base64Utils.decode(privateKey);
+        PKCS8EncodedKeySpec pkcs8KeySpec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
+        Key privateK = keyFactory.generatePrivate(pkcs8KeySpec);
+        Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
+        cipher.init(Cipher.ENCRYPT_MODE, privateK);
+        int inputLen = data.length;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        int offSet = 0;
+        byte[] cache;
+        int i = 0;
+        // 对数据分段加密
+        while (inputLen - offSet > 0) {
+            if (inputLen - offSet > MAX_ENCRYPT_BLOCK) {
+                cache = cipher.doFinal(data, offSet, MAX_ENCRYPT_BLOCK);
+            } else {
+                cache = cipher.doFinal(data, offSet, inputLen - offSet);
+            }
+            out.write(cache, 0, cache.length);
+            i++;
+            offSet = i * MAX_ENCRYPT_BLOCK;
+        }
+        byte[] encryptedData = out.toByteArray();
+        out.close();
+        return new String(encryptedData, charset);
+    }
     /**
      * <p>
      * 获取私钥
      * </p>
      *
-     * @param keyMap 密钥对
+     * @param keyMap
+     *            密钥对
      * @return
-     * @throws Exception
      */
     public static String getPrivateKey(Map<String, Object> keyMap) {
         Key key = (Key) keyMap.get(PRIVATE_KEY);
@@ -168,9 +249,9 @@ public class RSAEncryptUtils {
      * 获取公钥
      * </p>
      *
-     * @param keyMap 密钥对
+     * @param keyMap
+     *            密钥对
      * @return
-     * @throws Exception
      */
     public static String getPublicKey(Map<String, Object> keyMap) {
         Key key = (Key) keyMap.get(PUBLIC_KEY);
