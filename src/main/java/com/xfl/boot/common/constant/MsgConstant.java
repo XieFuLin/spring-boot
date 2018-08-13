54e032223742bda74682debbe191ac091b56ae22
XFL <18720909751@163.com>
2018-08-13 23:48:32 +0800

    代码提交

--- src/main/java/com/xfl/boot/common/constant/MsgConstant.java
+++ src/main/java/com/xfl/boot/common/constant/MsgConstant.java
@@ -6,5 +6,27 @@ package com.xfl.boot.common.constant;
  * description:
  */
 public class MsgConstant {
+    private Integer status;
+    private String message;
 
+    public MsgConstant(Integer status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
