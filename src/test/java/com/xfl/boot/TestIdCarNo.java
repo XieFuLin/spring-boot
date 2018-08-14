373ddd00425c8387bf8716fc539afb468206bbfd
XFL <18720909751@163.com>
2018-08-15 00:09:07 +0800

    添加测试类

--- src/test/java/com/xfl/boot/TestIdCarNo.java
+++ src/test/java/com/xfl/boot/TestIdCarNo.java
@@ -1,9 +1,20 @@
 package com.xfl.boot;
 
+import com.xfl.boot.common.utils.IdCardVerification;
+
+import java.text.ParseException;
+
 /**
  * Created by XFL
  * time on 2018/8/14 23:53
  * description:
  */
 public class TestIdCarNo {
+    public static void main(String[] args) {
+        try {
+            System.out.println(IdCardVerification.IDCardValidate("662662663256326"));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
 }
